3818ff41d5de77f206c3efb838a66eb28eebb1dd
shauniebyrne <byr21002@byui.edu>
2022-09-17 21:01:38 -0700

    almost

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week1/index.html"
+    }
+  ]
+
+function loadIndex() {
+    const ol = document.getElementById("linkslist");
+
+    links.forEach( link => {
+        const li = document.createElement("li");
+        const href = document.createElement("a");
+        href.setAttribute("href", link.url);
+        href.innerText = link.label;
+
+        li.appendChild(href);
+        ol.appendChild(li);
+    });
+};
