1243e5cdc192f5eeeef53b631411936cc4872413
shauniebyrne <byr21002@byui.edu>
2022-09-23 18:19:59 -0700

    week2notes

--- week2/quiz_ninja_project/main.js
+++ week2/quiz_ninja_project/main.js
@@ -3,28 +3,50 @@
 //const answer = prompt(question);
 //alert(`You answered ${answer}`);
 
-//creating a nested array
+//creating a nested map
 const quiz = new Map([
     ["What is Superman's real name?", "Clark Kent"],
     ["What is Wonder Woman's real name?", "Diana Prince"],
     ["What is Batman's real name?", "Bruce Wayne"]
 ]);
 
-let score = 0;
-//for-of loop to get values of the array
-for(const [question,answer] of quiz.entries()){
-    //get answer from user
-    const response = prompt(question);
-    //check if answer is correct
-    if(response === answer){
-        alert('Correct!');
-        //increase score by 1
-        score++;
-    } else {
-        alert(`Wrong! The correct answer was ${answer}.`);
-    }
-} 
+//main game function
+function start(quiz) {
+    let score = 0;
 
-//end of game, report the player's score
-alert(`Game Over, you scored ${score} point${score > 1 ? 's' : ''}`);
-//ternary operator (?) used at the end of this code - instead of a nother if/else statement
+    //main game loop
+    //for-of loop to get values of the array
+    for(const [question,answer] of quiz.entries()){
+        //get answer from user
+        const response = ask(question);
+        check(response,answer);
+    }
+
+    //call gameOver function
+    gameOver();
+
+    //these other functions need to be in the main start function
+    //because they use the same varibles as teh start(quiz) function
+    //function declaration
+    function ask(question){
+        return prompt(question);
+    }
+
+    function check(response,answer){
+    //check if answer is correct
+        if(response === answer){
+            alert('Correct!');
+            //increase score by 1
+            score++;
+        } else {
+            alert(`Wrong! The correct answer was ${answer}.`);
+        }
+    } 
+
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score > 1 ? 's' : ''}`);
+    }
+}
+
+//invoke start() function with quiz map as argument, needed to run the function and start the quiz
+start(quiz);
