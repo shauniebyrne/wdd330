0222d66d7142aac9239cc838f975a5082338fb6d
shauniebyrne <byr21002@byui.edu>
2022-10-13 14:43:23 -0700

    getting closer

--- todo_challenge/js/todos.js
+++ todo_challenge/js/todos.js
@@ -6,20 +6,30 @@ const myList = document.querySelector('.list');
 addTaskButton.addEventListener('click', function() {
     let input = myInput.value;
     myInput.value = '';
-    const checkButton = document.createAttribute('button');
+    const checkButton = document.createElement('button');
     const listValue = document.createElement('li');
     const deleteButton = document.createElement('button');
+    const allTogether = document.createElement('li');
+    allTogether.classList.add('border-help');
 
+    //create input for buttons and list
     checkButton.innerHTML = '&#9745;';
     listValue.textContent = input;
     deleteButton.innerHTML = '&#9746;';
 
-    listValue.appendChild(deleteButton);
-    myList.appendChild(listValue);
+    //place all buttons and value in an li then in ul
+    allTogether.appendChild(checkButton);
+    allTogether.appendChild(listValue);
+    allTogether.appendChild(deleteButton);
+    myList.appendChild(allTogether);
 
     deleteButton.addEventListener('click', function() {
-        myList.removeChild(listValue);
+        myList.removeChild(allTogether);
     });
+
+    checkButton.addEventListener('click', function() {
+        listValue.classList.add('cross-out');
+    })
 
     myInput.focus();
 })
