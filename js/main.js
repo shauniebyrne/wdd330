04741e1d697963ab7568f06b8bdb71911c4bfb17
shauniebyrne <byr21002@byui.edu>
2022-11-17 12:04:28 -0700

    half way

--- js/main.js
+++ js/main.js
@@ -34,6 +34,14 @@ const links = [
     {
       label: "Week 9 Notes",
       url: "week9/index.html"
+    },
+    {
+      label: "Week 10 Notes",
+      url: "week10/index.html"
+    },
+    {
+      label: "Final Project",
+      url: "final_project/index.html"
     }
   ]
 
